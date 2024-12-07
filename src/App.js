79f2ad0bd90533b51188abdc87e4dc9f1f027ab9
pawan <pawanjalandhara@gmail.com>
2024-12-07 20:56:37 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import NavBar from "./Components/NavBar";
 
-import NavBar from './Advanceproject/NavBar';
-import Siderbar from './Advanceproject/Siderbar';
-import './App.css';
-
-
-function App() {
+const App = () => {
   return (
-    <>
-    <NavBar/>
-    <Siderbar/>
-   
-    </>
+    <div>
+      <nav>
+        <NavBar/>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/About">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/About" element={<About/>} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This page provides information about our website.This page provides information about our website.This page provides information about our website.</p>
+    </div>
+  );
+};
+
+export default About;
