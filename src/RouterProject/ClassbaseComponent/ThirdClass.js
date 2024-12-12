7454bf2345031150c119d1a26763483dc49ab21b
pawan <pawanjalandhara@gmail.com>
2024-12-12 18:41:14 +0530

    update with button

--- src/RouterProject/ClassbaseComponent/ThirdClass.js
+++ src/RouterProject/ClassbaseComponent/ThirdClass.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-// import img from "../About.svg";
-
 export default class ThirdClass extends Component {
   constructor() {
     super();
@@ -12,22 +10,25 @@ export default class ThirdClass extends Component {
     this.state = {
       article: [],
       loading: false,
+      page: 1, 
     };
   }
 
   componentDidMount() {
-    const url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=784dc79105f444078c2a012390d60bc5";
-    console.log("Fetching data from URL:", url);
+    this.fetchNews();
+  }
+
+  fetchNews = () => {
+    const { page } = this.state;
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=784dc79105f444078c2a012390d60bc5&page=${page}`;
 
     this.setState({ loading: true });
 
     axios
       .get(url)
       .then((response) => {
-        // console.log("Response data:", response.data);
         this.setState({
-          article: response.data.articles , 
+          article: response.data.articles,
           loading: false,
         });
       })
@@ -35,32 +36,61 @@ export default class ThirdClass extends Component {
         console.error("Error fetching news data:", error);
         this.setState({ loading: false });
       });
-  }
+  };
+
+  handlePreClick = () => {
+    this.setState(
+      (prevState) => ({ page: prevState.page - 1 }),
+      this.fetchNews   
+    );
+  };
+
+  handleNextClick = () => {
+    this.setState(
+      (NextState) => ({ page: NextState.page + 1 }),
+      this.fetchNews 
+    );
+  };
 
   render() {
-    const { article, loading } = this.state;
+    const { article, loading, page } = this.state;
 
     return (
-      <div>
-        {loading ? (
-           <div class="loading-circle"></div>
-        ) : (
-          <div className="box">
-            {
-              article.map((item, index) => (
+      <>
+        <div>
+          {loading ? (
+            <div className="loading-circle"></div>
+          ) : (
+            <div className="box">
+              {article.map((item, index) => (
                 <div className="boxAlign" key={index}>
                   <h4>{item.title}</h4>
                   {item.urlToImage && (
-                    <img src={item.urlToImage} alt={item.title} className="img" />
+                    <img
+                      src={item.urlToImage}
+                      alt={item.title}
+                      className="img"
+                    />
                   )}
                   <p>{item.description}</p>
                 </div>
-              ))
-            
-            }
-          </div>
-        )}
-      </div>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="btnbox">
+          <button
+            className="btn"
+            disabled={page <= 1}
+            onClick={this.handlePreClick}
+          >
+            ← Previous
+          </button>
+          <button  disabled={page >=3} className="btn" onClick={this.handleNextClick}>
+            Next →
+          </button>
+        </div>
+      </>
     );
   }
 }
